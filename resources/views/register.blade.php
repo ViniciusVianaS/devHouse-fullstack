2263d28198d1c09b6b153ca40309b2fddf4e5c36
vinicius viana <viniciuspro333@gmail.com>
2024-06-30 00:36:03 -0300

    feat: Creation of the registration layout and creation of Migration

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,33 +3,35 @@
 @section('title' . 'DevHouse')
 @vite('resources/css/app.css')
 @section('content')
-    <div class="">
-        <h1>Cadastre a casa</h1>
-        <form action="/views" method="post" enctype="multipart/form-data">
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-semibold">Cadastre a casa</h1>
+        <form class="mt-6" action="/views" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-                <label for="image">Imagem da casa:</label>
-                <input type="file" id="image" name="image" class="form-control-file" required>
+            <div>
+                <label for="image" class="block text-gray-700 font-bold mb-2">Imagem da casa:</label>
+                <input type="file" id="image" name="image" class="block w-full text-gray-700 border rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" required>
             </div>
             <div class="form-group">
-                <label for="nome">Nome da casa</label>
-                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome do evento">
+                <label for="nome" class="block text-gray-700 font-bold mb-2">Nome da casa</label>
+                <input type="text" class="block w-full text-gray-700 border rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="nome" name="nome" placeholder="Nome da casa" required>
             </div>
             <div class="form-group">
-                <label for="valor">Valor:</label>
-                <input type="text" class="form-control" id="valor" name="valor" required>
+                <label for="valor"class="block text-gray-700 font-bold mb-2">Valor:</label>
+                <input type="text" class="block w-full text-gray-700 border rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="valor" name="valor" placeholder="Cidade da casa" required>
             </div>
             <div class="form-group">
-                <label for="cidade">Cidade:</label>
-                <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Local do evento"
+                <label for="cidade" class="block text-gray-700 font-bold mb-2">Cidade:</label>
+                <input type="text" class="block w-full text-gray-700 border rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="cidade" name="cidade" placeholder="Cidade da casa"
                     required>
             </div>
             <div class="form-group">
-                <label for="descricao">Cidade:</label>
-                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Local do evento"
+                <label for="descricao" class="block text-gray-700 font-bold mb-2">Descrição:</label>
+                <input type="text" class="block w-full text-gray-700 border rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="descricao" name="descricao" placeholder="Descrição da casa"
                     required>
             </div>
-            <input type="submit" class="btn btn-primary" value="Cadastrar">
+            <div class="flex items-center justify-center mt-6">
+                <input type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="Cadastrar">
+            </div>
         </form>
     </div>
 
