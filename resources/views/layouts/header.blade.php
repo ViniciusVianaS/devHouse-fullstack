c80892d0f6abee12681eabaab278b047b20f2408
vinicius viana <viniciuspro333@gmail.com>
2024-07-07 15:01:55 -0300

    feat: adding the jetstream library for user authentication

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,24 +14,50 @@
         <header class="bg-white py-5 px-3 w-full top-0 flex flex-row items-center justify-between">
             <div class="flex items-center bg-slate-100 px-3 py-2 w-full max-w-[300px] lg:max-w-[50%] rounded-lg lg:mr-8">
                 <i class="fa fa-search text-gray-500" style="font-size:16px"></i>
-                <input type="text" class="w-full bg-slate-100 px-2 outline-none" placeholder="Pesquise aqui">
+                <input type="text" class="w-full bg-slate-100 px-2 border-none focus:outline-none"
+                    placeholder="Pesquise aqui">
             </div>
-            <nav class="mr-auto">
-                <a href="/"
+            <nav class="mr-auto ml-3">
+                <a href="/" title="Home"
                     class="text-gray-500 font-medium hover:text-black hover:bg-gray-100 p-2 rounded-lg duration-300">
                     <i class="fa fa-home" style="font-size: 26px;"></i>
                 </a>
-                <a href="/register"
+                <a href="/create" title="Cadastrar casa"
                     class="text-gray-500 font-medium hover:text-black hover:bg-gray-100 p-2 rounded-lg duration-300 mt-[2px] md:ml-4 ml-1">
                     <i class="fa fa-plus" style="font-size: 22px;"></i>
                 </a>
             </nav>
             <div class="flex gap-3 items-center">
-                <button
-                    class="bg-rose-500 hover:bg-rose-600 duration-300 text-white flex items-center justify-center gap-2 px-4 py-2 rounded font-medium ml-3">
-                    <span class="hidden md:flex">Sign In</span>
-                    <i class="fa fa-user  md:ml-1" style="font-size:18px"></i>
-                </button>
+                @auth
+                    <a href="/dashboard" title="Minhas casas"
+                        class="bg-sky-500 hover:bg-sky-600 cursor-pointer duration-300 text-white flex items-center justify-center gap-2 px-4 py-2 rounded font-medium ml-3">
+                        <span class="hidden md:flex">Minhas casas</span>
+                        <i class="fa fa-home  md:ml-1" style="font-size:18px"></i>
+                    </a>
+                    <li class="list-none">
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <a href="/logout" title="Sair" 
+                             onclick="event.preventDefault();
+                             this.closest('form').submit();"
+                             class="bg-rose-500 hover:bg-rose-600 cursor-pointer duration-300 text-white flex items-center justify-center gap-2 px-4 py-2 rounded font-medium ml-3">
+                                <span class="hidden md:flex">Sair</span>
+                            </a>
+                        </form>
+                    </li>
+                @endauth
+                @guest
+                    <a href="/login" title="Login"
+                        class="bg-rose-500 hover:bg-rose-600 cursor-pointer duration-300 text-white flex items-center justify-center gap-2 px-4 py-2 rounded font-medium ml-3">
+                        <span class="hidden md:flex">Login</span>
+                        <i class="fa fa-user  md:ml-1" style="font-size:18px"></i>
+                    </a>
+                    <a href="/register" title="Cadastrar"
+                        class="bg-sky-500 hover:bg-sky-600 cursor-pointer duration-300 text-white flex items-center justify-center gap-2 px-4 py-2 rounded font-medium ml-3">
+                        <span class="hidden md:flex">Cadastrar-se</span>
+                        <i class="fa fa-plus  md:ml-1" style="font-size:18px"></i>
+                    </a>
+                @endguest
             </div>
         </header>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HouseController::class, 'index'])->name('home-index');
 
-Route::get('/register', function () {
-    return view('register');
+Route::get('/create', function () {
+    return view('create');
 });
 
 Route::post('/houses', [HouseController::class, 'store'])->name('house-store');
@@ -14,3 +14,13 @@ Route::get('/houses/{id}', [HouseController::class, 'show'])->name('house-show')
 Route::get('/edit/{id}', [HouseController::class, 'edit']);
 Route::put('/update/{id}', [HouseController::class, 'update'])->name('house-update');
 Route::delete('/houses/{id}', [HouseController::class, 'destroy']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
